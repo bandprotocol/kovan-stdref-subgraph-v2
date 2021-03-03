51340bc7526eea66a05d9e2684c1fba399aa279f
Panu <Panu.suk@protonmail.com>
2021-03-03 16:54:28 +0700

    Add heigh and timestamp

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,13 +1,25 @@
 import { GetReferenceDataCall, GetReferenceDataBulkCall } from "../generated/StdReferenceProxy/StdReferenceProxy"
 import { GetReferenceData } from "../generated/schema"
 
-export function handleGetReferenceData(
-  call: GetReferenceDataCall
-): void {
+import { ethereum } from "@graphprotocol/graph-ts"
+
+function NewGetReferenceData(
+  call: ethereum.Call
+): GetReferenceData {
   let id = call.transaction.hash.toHex();
 
   let entity = new GetReferenceData(id);
-  entity.from = call.transaction.from;
+  entity.from = call.from;
+  entity.height = call.block.number.toI32();
+  entity.timestamp = call.block.timestamp.toI32();
+
+  return entity;
+}
+
+export function handleGetReferenceData(
+  call: GetReferenceDataCall
+): void {
+  let entity = NewGetReferenceData(call);
   entity.symbols = [call.inputs._quote];
 
   entity.save();
@@ -16,10 +28,7 @@ export function handleGetReferenceData(
 export function handleGetReferenceDataBulk(
   call: GetReferenceDataBulkCall
 ): void {
-  let id = call.transaction.hash.toHex();
-
-  let entity = new GetReferenceData(id);
-  entity.from = call.transaction.from;
+  let entity = NewGetReferenceData(call);
   entity.symbols = call.inputs._quotes;
 
   entity.save();
